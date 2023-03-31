df7dfbd35253186ba6428930078340d7dbaddd69
Andres Torres <andrestor2@gmail.com>
2023-03-30 22:05:12 -0500

    FEAT: Added spring webflux dependency to continue.

--- sales-service/src/main/java/io/andreslond/salesservice/sale/infraestructure/in/web/CreateSaleController.java
+++ sales-service/src/main/java/io/andreslond/salesservice/sale/infraestructure/in/web/CreateSaleController.java
@@ -1,5 +1,9 @@
 package io.andreslond.salesservice.sale.infraestructure.in.web;
 
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class CreateSaleController {
+
+
 }
